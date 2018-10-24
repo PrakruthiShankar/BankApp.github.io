45fd193f7ea1d7fbc7e4cd9a9f02a4b55452bfd6
Prakruthi Shankar <prakruthishankar@Prakruthis-MacBook-Pro.local>
2018-10-24 22:51:21 +0530

    basic structure

---
+++ build/precache-manifest.b0a1190c3b10da942abe31da41e6eb0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2afebcc1c58b255c7d7",
+    "url": "/static/css/main.96a360be.chunk.css"
+  },
+  {
+    "revision": "e2afebcc1c58b255c7d7",
+    "url": "/static/js/main.e2afebcc.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3deb78fc5b927732c1faacb76c00cc1f",
+    "url": "/index.html"
+  }
+];
